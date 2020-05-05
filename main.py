e622d9abdae25703d1ca93301c1dadb72968e5b8
iMay <33700021+imay97@users.noreply.github.com>
2020-05-05 19:50:27 +0300

    dev

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def callback_inline(call):
         f.write(str(msg.message_id))
 
 def follow(id):
-    cursor.execute('SELECT * FROM *')
+    cursor.execute('SELECT * FROM "test"')
     msg = bot.send_message(call.message.chat.id, cursor.fetchone(), reply_markup = key_main())
 
 #end
