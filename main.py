3555d568e8d495ce5a561c9d491e8a6dfd59e940
iMay <33700021+imay97@users.noreply.github.com>
2020-05-09 17:23:54 +0300

    dev

--- main.py
+++ main.py
@@ -76,9 +76,10 @@ def start(message):
             Тогда тебе к нам. С нами ты получишь стабильный заработок,
             сидя дома и играя в доту, забудешь что такое кредиты и финансовые проблемы.
             Жми \"Заработать\" и делай свои первые деньги.''', reply_markup = key_main())
+            hash = hashlib.md5(str(id).encode())
             cur.execute("INSERT INTO users (id, name, date, msg, ref) VALUES (%s, %s, %s, %s, %s)",
             (int(message.chat.id), str(message.chat.last_name + ' ' + message.chat.first_name),
-            datetime.datetime.today().strftime('%Y-%m-%d-%H.%M.%S'), int(msg.message_id), str(hashlib.md5(str(id).encode()))))
+            datetime.datetime.today().strftime('%Y-%m-%d-%H.%M.%S'), int(msg.message_id), str(hash.hexdigest())))
             conn.commit()
 
 @bot.message_handler(content_types=['text'])
