ec5c13c9588da753519a9b78c93c83efac95b9d8
iMay <33700021+imay97@users.noreply.github.com>
2020-05-10 21:28:49 +0300

    dev

--- main.py
+++ main.py
@@ -123,9 +123,10 @@ def callback_inline(call):
             then = datetime.datetime.strptime(str(cur.fetchone()[0]), '%H.%M.%S')
             delta = now - then
             if(delta.seconds < 3600):
-                bot.send_message(id, 'Просмотр записей будет доступен через ' + str((delta.seconds / 60) - (delta.seconds % 60)) + ' мин. ' + str(delta.seconds % 60) + ' сек.')
+                bot.send_message(id, 'Просмотр записей будет доступен через ' + delta.minutes + ' мин. ' + 1 + ' сек.')
+            else:
                 cur.execute('UPDATE users SET time = %s', (now.strftime('%H.%M.%S'),))
-            conn.commit()
+                conn.commit()
 
 def partners(id, func):
     with conn.cursor() as cur:
